3f70309f7888783037fd62cb907054264f4b36f8
Yash Jaiswal <jais.yash01@gmail.com>
2020-07-13 13:28:43 +0530

    made some pages

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <div>
+        <h2>About Page</h2>
+      </div>
+
+      <div>
+        <p>This Website is made by Yash Jaiswal</p>
+      </div>
+    </div>
+  )
+}
+
+export default About
--- pages/app.js
+++ pages/app.js
@@ -1,11 +1,121 @@
-import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
 
-function App() {
+export default function App() {
   return (
-    <div>
-      <h2>App Page</h2>
+    <div className="container">
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Home Page
+        </h1>
+
+        <div>
+          <ul>
+            <li>
+              <Link href="/app">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About Us</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+      </main>
+
+      <footer>
+        <a
+          href=""
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Made by{' '}
+          <p className="name">Yash Jaiswal</p>
+        </a>
+      </footer>
+
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer img {
+          margin-left: 0.5rem;
+        }
+
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .logo {
+          height: 1em;
+        }
+
+        .name{
+          padding: .2rem;
+          text-transform: uppercase;
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
     </div>
   )
 }
-
-export default App
